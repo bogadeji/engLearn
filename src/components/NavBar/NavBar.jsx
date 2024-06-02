6bdd595f9a0910cb44eadeb1ce20a70e34c06018
unknown <bogadeji@gmail.com>
2024-06-02 18:03:09 +0100

    Add links to the right pages

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,21 +4,30 @@ import HomeIcon from '../../assets/icons/HomeIcon';
 import Settingsicon from '../../assets/icons/SettingsIcon';
 import StatIcon from '../../assets/icons/StatIcon';
 import ProfileIcon from '../../assets/icons/ProfileIcon';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
   return (
     <nav className={styles.navbar}>
       <div className={[styles.navlink, styles.home].join(' ')}>
-        <HomeIcon />
+        <NavLink to="/">
+          <HomeIcon />
+        </NavLink>
       </div>
       <div className={[styles.navlink, styles.stats].join(' ')}>
+      <NavLink to="analytics">
         <StatIcon />
+      </NavLink>
       </div>
       <div className={[styles.navlink, styles.profile].join(' ')}>
+      <NavLink to="/user-profile">
         <ProfileIcon />
+      </NavLink>
       </div>
       <div className={[styles.navlink, styles.settings].join(' ')}>
-        <Settingsicon />
+      <NavLink to="/settings">
+      <Settingsicon />
+        </NavLink>
       </div>
     </nav>
   )
