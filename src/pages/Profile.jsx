2ea312cbf993cc0be5567e15799847ab972b9691
unknown <bogadeji@gmail.com>
2024-06-02 19:08:32 +0100

    Add text to Profile and Settings pages

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import styles from './pages.module.css'
 function Profile() {
   return (
     <>
+      <h3 className={styles.heading}>Profile</h3>
       <div className={styles.card}>
         <p className={styles.cardtext}>
           Coming Soon!
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -4,6 +4,7 @@ import styles from './pages.module.css'
 function Settings() {
   return (
     <>
+      <h3 className={styles.heading}>Settings</h3>
       <div className={styles.card}>
         <p className={styles.cardtext}>
           Coming Soon!
